f54abd12afb1ae91f3a60721c824f048a713c9da
Furhad Jidda <furhadjidda@gmail.com>
2017-01-10 07:50:01 -0600

    Si4703 - Adding a working example in app.c
    This change includess adding some i2c messages that get sent out from the app.

--- code/app.c
+++ code/app.c
@@ -12,6 +12,11 @@
 int main()
 {
 	int fd, err=0;
+	unsigned int nc = 0;
+	nc = 981; //this is 101.1 The Fox In Kansas City Classic Rock!!
+	nc *= 10;  //this math is for USA FM only
+	nc -= 8750;
+	nc /= 20;
 	
 	fd = open("/dev/i2cchar", O_RDWR);
 	if(fd < 0)
@@ -19,8 +24,28 @@ int main()
 		perror("open:");
 		return -1;
 	}
-	char stringToSend[4] = "hi!";
-	write(fd,stringToSend,strlen(stringToSend));
+
+
+	char list1[] = {0,0,0,0,0,0,0,0,0,0,129,0};
+	write(fd,list1,sizeof(list1));
+	sleep(1);
+
+	char list2[] = {64,1};
+	write(fd,list2,sizeof(list2));
+	sleep(1);
+
+	char list3[] = {64,1,0,0,0,0,0,1};
+	write(fd,list3,sizeof(list3));
+	sleep(1);
+
+	char list4[] = {64,1,128,nc};
+	write(fd,list4,sizeof(list4));
+	sleep(1);
+
+	char list5[] = {64,1,0,nc};
+	write(fd,list5,sizeof(list5));
+	sleep(1);
+
 	close(fd);
 	return 0;
 }
